495e8b8b6df087d463150b409be3fed653cc54b5
shanmugapriya222 <shanmugapriyab211@gmail.com>
2025-03-05 11:01:57 +0530

    commited

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import UpdateItem from "./components/UpdateItem";
 
 // use the following link to get the data
 // `/doors` will give you all the doors, to get a specific door use `/doors/1`.
-const API_URI = `http://${import.meta.env.VITE_API_URI}/doors`;
+const API_URI = `http://${import.meta.env.VITE_API_URI}/doors/:id`;
 
 function App() {
   // Get the existing item from the server
--- src/components/UpdateItem.jsx
+++ src/components/UpdateItem.jsx
@@ -1,11 +1,45 @@
+import React, {useState, useEffect} from "react";
+
+const API_URI=`http://${import.meta.env.VITE_API_URI}/doors`;
+const ITEM_ID="1";
+
 const UpdateItem = ({ item }) => {
     // 1. Create a state for the form
+    const[updatedValue, setUpdatedValue]=useState(item?.name||"")
+    const[message, setMessage]=useState("");
     // 2. Create a function to handle the form submission
-    // 3. Create a function to handle the form input changes
+    const handleChange=(e)=>{
+        setUpdatedValue(e.target.value);
+    };
 
+    // 3. Create a function to handle the form input changes
+    const handleUpdate=()=>{
+        fetch(`http://localhost:8000/doors/${item.id}`,{
+            method:"PUT",
+            headers:{
+                "Conent-Type":"application/json",
+            },
+            body: JSON.stringify({name: updatedValue}),
+        })
+        .then((res)=>res.json())
+        .then((data)=>{
+            setMessage("Item updated sucessfully!");
+        })
+        .catch((err)=>{
+            console.log("Error: ", err);
+            setMessage("Error updating item. ")
+        });
+    };
     // your code here
-    return null;
+
+    return(
+        <div>
+            <h2>Updated Item</h2>
+            <input type="text" value={updatedValue} onChange={handleChange}/>
+            <button onClick={handleUpdate}>Update</button>
+            {message &&<p>{message}</p>}
+        </div>
+    );
 };
 
 export default UpdateItem;
-
